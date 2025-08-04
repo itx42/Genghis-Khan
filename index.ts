b4efa7267a466793c5b90f9f7fb10e143c2861ea
itx42 <naoiro42@gmail.com>
2025-08-05 01:04:20 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,22 @@
-import { createSonolusServer } from 'sonolus-express'
-import { EngineData } from './engine'
-import { levels } from './levels'
+import { serve } from 'sonolus-express'
 
-export default createSonolusServer({
-  engine: EngineData,
-  levels,
+export default serve({
+  levels: [],
+  engine: {
+    item: {
+      name: 'pjsekai',
+      version: '1.0',
+      title: { en: 'Project Sekai Engine', ja: 'プロセカエンジン' },
+      author: { en: 'You', ja: 'あなた' },
+      description: { en: '', ja: '' },
+      thumbnailUrl: '/engine/pjsekai/thumbnail.webp',
+    },
+    data: {
+      scriptUrl: '/engine/pjsekai/script.js',
+      configurationUrl: '/engine/pjsekai/configuration.json',
+      thumbnailUrl: '/engine/pjsekai/thumbnail.webp',
+      previewUrl: '/engine/pjsekai/preview.mp3',
+    },
+    localization: { strings: [] },
+  }
 })
